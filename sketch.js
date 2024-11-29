1a12232a35a2e4bdd5ad776935fa53b33ab4298e
petrusferreira <00001139504459SP@al.educacao.sp.gov.br>
2024-11-29 18:37:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+let musica;
+
+function preload(){
+musica = loadSound("fundo.mp3")
+}
+
+function setup(){
+musica.play();
+ musica.loop();
+ }
+
+const avança = document.querySelectorAll(".proximo");
+avança.forEach(button =>{
+ button.addEventListener('click', function(){
+  const atual = document.querySelector('.ativo');
+   const prximoPasso = 'passo-' + 
+    this.getAttribute('data-proximo');
+     atual.classList.remove('ativo');
+  
+document.getElementById(proximoPasso).classList.add('atvo');
+
+})               
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
